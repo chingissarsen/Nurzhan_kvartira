4ba5ee21e573163e6617dc437d27941c481bb896
chingissarsen <chingissarsen@gmail.com>
2026-01-13 19:27:52 +0500

    upload

--- data.js
+++ data.js
@@ -28,7 +28,52 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.1166068348255482,
+          "pitch": 0.13707673121858122,
+          "rotation": 0,
+          "target": "1-_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-_01",
+      "name": "Кухня_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.447408946937344,
+          "pitch": 0.08955571774359683,
+          "rotation": 0.7853981633974483,
+          "target": "0-_01_01"
+        }
+      ],
       "infoHotspots": []
     }
   ],
